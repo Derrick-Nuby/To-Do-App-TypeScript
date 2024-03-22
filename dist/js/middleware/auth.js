e914ed04d29b252704b781a02c351b2e4adc15c0
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-03-22 13:14:28 +0200

    compiled tsc middlewares

---
+++ dist/js/middleware/auth.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authJWT = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const authJWT = (req, res, next) => {
+    var _a;
+    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+    if (token) {
+        jsonwebtoken_1.default.verify(token, 'secretString', (err) => {
+            if (err) {
+                return res.status(403).json({ message: 'Failed to authenticate token' });
+            }
+            next();
+        });
+    }
+    else {
+        res.status(401).json({ message: 'No token provided' });
+    }
+};
+exports.authJWT = authJWT;
