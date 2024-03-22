97c4352544432a5f5c59a37a25a3690075da5f44
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-03-22 13:14:11 +0200

    compiled controllers

---
+++ dist/js/controllers/auth.js
@@ -0,0 +1,57 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loginUser = exports.createAccount = void 0;
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const user_1 = __importDefault(require("../models/user"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const createAccount = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const body = req.body;
+        const user = new user_1.default({
+            name: body.name,
+            email: body.email,
+            password: body.password,
+        });
+        const newUser = yield user.save();
+        res
+            .status(201)
+            .json({ message: "User Created Successfuly", user: newUser });
+    }
+    catch (error) {
+        throw error;
+    }
+});
+exports.createAccount = createAccount;
+const loginUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { email, password } = req.body;
+        const user = yield user_1.default.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const isPasswordValid = yield bcryptjs_1.default.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        const token = jsonwebtoken_1.default.sign({ id: user.id, username: user.name, email: user.email }, 'secretString', { expiresIn: '1h' });
+        res.status(200).json({ message: "User logged in successfully", user, token });
+    }
+    catch (error) {
+        // Handle any errors
+        console.error("Error logging in:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+exports.loginUser = loginUser;
