055401eb9599f5e84b86b920b065e081fc14c8d0
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-06-20 12:46:09 +0200

    deleted dist

--- dist/js/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const validation_1 = require("../middleware/validation");
-const router = (0, express_1.Router)();
-router.post("/create", validation_1.validateUserRegister, auth_1.createAccount);
-router.post("/login", validation_1.validateUserLogin, auth_1.loginUser);
-exports.default = router;
